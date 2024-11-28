36a4d8ee3a5f35c8b315246ca81672eda3f34b14
ihrimon <ihrimon147@gmail.com>
2024-11-28 08:35:56 +0600

    express method application request added

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// app.use(express.static(`${__dirname}/public`, {
+//     index: 'home.html'
+// }));
+
+// app.get('/', (req, res) => {
+//   res.send('This is home page');
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('Home page with post request');
+// });
+
+// app.listen(5000, () => {
+//   console.log(`server is running on port at 5000`);
+// });
